ef77b4b73a2d64dc281c9de3c976e482b7e20b5a
ngarciaotero <nasleygarciaotero@gmail.com>
2024-04-23 22:16:11 -0400

    Add content to homepage and style
    Add bkgd img and style content div
    
    Add robot component
    
    Update header and description txt
    
    Add intro-header class to change font color
    
    Display container floating left centered
    
    Set body to flex to allow container to fill empty space

--- src/homePage.js
+++ src/homePage.js
@@ -1,17 +1,27 @@
+import restaurant from "./futuristic-space.jpg";
+import robot from "./robot.png";
+
 const createHomePage = () => {
   const contentDiv = document.querySelector("#content");
+  contentDiv.style.background = `url(${restaurant})`;
 
   const introContainer = document.createElement("div");
   introContainer.classList.add("intro-container");
 
   const introHeader = document.createElement("h1");
-  introHeader.textContent = "Lorem ipsum dolor";
+  introHeader.classList.add("intro-header");
+  introHeader.textContent = "NeonNexus Bistro";
   introContainer.appendChild(introHeader);
 
   const description = document.createElement("p");
   description.textContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est velit egestas. Commodo odio aenean sed adipiscing diam donec adipiscing. Mauris vitae ultricies leo integer. Tristique et egestas quis ipsum suspendisse ultrices gravida. Et ultrices neque ornare aenean. In nisl nisi scelerisque eu. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend. Odio facilisis mauris sit amet massa. Ac tincidunt vitae semper quis. Mattis aliquam faucibus purus in massa tempor. Euismod quis viverra nibh cras pulvinar mattis nunc sed. Eget nulla facilisi etiam dignissim. Consequat mauris nunc congue nisi vitae suscipit tellus mauris.";
+    "Welcome traveler! May I offer you a Stardust Spritzer or Quantum Quencher from our menu. Try our Cybernetic Sushi Symphony, from our award winning robo chefs. In our cyberpunk oasis, we blend flavors and styles to create an unforgettable dining experience. Join us and discover the future of culinary artistry";
   introContainer.appendChild(description);
+
+  const robotCharacter = document.createElement("img");
+  robotCharacter.classList.add("intro-robot");
+  robotCharacter.src = robot;
+  introContainer.appendChild(robotCharacter);
 
   contentDiv.appendChild(introContainer);
 };
