7a9e647fe3c849b95651d523d27ed6ee54d06c40
ngarciaotero <nasleygarciaotero@gmail.com>
2024-04-25 20:48:56 -0400

    create menu container

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,6 +1,15 @@
 const createMenuPage = () => {
   const contentDiv = document.querySelector("#content");
   contentDiv.setAttribute("class", "menu-bkgrd");
+  
+  const menuContainer = createMenuContainer();
+
+};
+
+const createMenuContainer = () => {
+  const menuContainer = document.createElement("div");
+  menuContainer.classList.add("menu-container");
+  return menuContainer;
 };
 
 export default createMenuPage;
